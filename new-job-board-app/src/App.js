b54bde29a86851884a6c335e3ab44bc3e3902e86
Hari Rajan <harirajan@fb.com>
2020-12-21 11:21:05 -0800

    removing silly console logs

--- new-job-board-app/src/App.js
+++ new-job-board-app/src/App.js
@@ -7,18 +7,13 @@ import React, { useState } from "react";
 function App() {
   const [helloMessage, setHelloMessage] = useState("");
   const handleButtonClick = () => {
-    console.log("here");
     getHelloWorldMessageService()
-      // .then((response) => {
-      //   response.json().then((data) => {
-      //     console.log("here");
-      //     console.log(data);
-      //   });
-      // })
-      .then(r => r.text())
-      .then(t => console.log(t))
+      .then((response) => {
+        response.json().then((data) => {
+          console.log(data);
+        });
+      })
       .catch((error) => {
-        console.log("error");
         console.log(error);
       });
   };
