c8433ad11363602c8c2c3a61873d332aedae5b57
Kyle Berlind <kyleberlind@duke.edu>
2020-12-21 00:17:07 -0700

    added button to connect to back end

--- new-job-board-api/api/views/hello_world_view.py
+++ new-job-board-api/api/views/hello_world_view.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, jsonify
 
-main = Blueprint('main', __name__)
 
+main = Blueprint('main', __name__)
 
 @main.route('/get_hello_world', methods=['GET'])
 def get_hello_world():
-    return "Hello World"
+     return jsonify({'message': 'Hello World'})
--- new-job-board-app/src/App.js
+++ new-job-board-app/src/App.js
@@ -1,23 +1,26 @@
-
-import logo from './logo.svg';
-import {getHelloWorldMessageService} from "./services/HelloWorldServices"
-import LoginContainer from './login/LoginContainer.react.js';
-import './App.css';
+import logo from "./logo.svg";
+import { getHelloWorldMessageService } from "./services/HelloWorldServices";
+import LoginContainer from "./login/LoginContainer.react.js";
+import "./App.css";
 import React, { useState } from "react";
 
 function App() {
   const [helloMessage, setHelloMessage] = useState("");
   const handleButtonClick = () => {
     getHelloWorldMessageService()
-    .then((response) => {
-      console.log(response)
-    }).catch((error) => {
-      console.log(error)
-    });
+      .then((response) => {
+        response.json().then((data) => {
+          console.log(data);
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
   return (
     <div className="App">
       <LoginContainer />
+      <button onClick={handleButtonClick}>click to connect to backend </button>
     </div>
   );
 }
