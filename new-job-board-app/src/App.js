b8376223e8797ff05f32fff224c593c628ac9752
Hari Rajan <harirajan@fb.com>
2020-12-21 11:17:26 -0800

    adding venv

--- new-job-board-app/src/App.js
+++ new-job-board-app/src/App.js
@@ -7,13 +7,18 @@ import React, { useState } from "react";
 function App() {
   const [helloMessage, setHelloMessage] = useState("");
   const handleButtonClick = () => {
+    console.log("here");
     getHelloWorldMessageService()
-      .then((response) => {
-        response.json().then((data) => {
-          console.log(data);
-        });
-      })
+      // .then((response) => {
+      //   response.json().then((data) => {
+      //     console.log("here");
+      //     console.log(data);
+      //   });
+      // })
+      .then(r => r.text())
+      .then(t => console.log(t))
       .catch((error) => {
+        console.log("error");
         console.log(error);
       });
   };
