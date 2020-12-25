e9dd30ae7f25a88783ef259cffd7cb7a0df71399
Kyle Berlind <kyleberlind@duke.edu>
2020-12-24 19:19:27 -0700

    added to the pip file

---
+++ databases/database-1/user/tbl_user.sql
@@ -0,0 +1,19 @@
+use user;
+
+create table tbl_user (
+	id INT NOT NULL AUTO_INCREMENT,
+	password NVARCHAR(128) NOT NULL,
+	first_name NVARCHAR(64),
+	last_name NVARCHAR(64),
+	email_address NVARCHAR(64) NOT NULL,
+    user_type INT NOT NULL,
+    sign_up_date datetime NOT NULL,
+    primary key (id)
+);
+
+CREATE INDEX user_id_email_address_index
+on tbl_user (id, email_address);
+
+CREATE UNIQUE INDEX unique_email_address_index
+on tbl_user (email_address);
+
